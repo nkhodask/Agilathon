39ab7f099c414298bd6d56b808a727cc4c21694c
niket.anup.khodaskar <Niket.Khodaskar@aexp.com>
2017-06-13 14:33:25 +0530

    deleted target folder

--- target/classes/data-h2.sql
+++
@@ -1,5 +0,0 @@
-insert into policy.policy_details values(1,'LIC','Life Insurance',10);
-insert into policy.policy_details values(2,'LIC - Jeevan Beema','Life Insurance',10);
-insert into policy.policy_details values(3,'Tata AIC','Life Insurance',10);
-insert into policy.policy_details values(4,'Bajaj Allianz','Life Insurance',10);
-
