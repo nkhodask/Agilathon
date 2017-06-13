74eaf07bb28000365484e0362ad66e56cfa5a4bc
Bunty raghani <b_raghani@yahoo.com>
2017-06-13 00:36:50 -0700

    Added Test Cases

---
+++ src/test/java/com/demo/policy/policydetails/service/FetchPolicyDetailsServiceImplTest.java
@@ -0,0 +1,19 @@
+package com.demo.policy.policydetails.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by b.mukeshlal.raghani on 6/12/2017.
+ */
+public class FetchPolicyDetailsServiceImplTest {
+
+    @Test
+    public void testSampleTestCase()
+    {
+        boolean result = true;
+
+        assertTrue(result);
+    }
+}
