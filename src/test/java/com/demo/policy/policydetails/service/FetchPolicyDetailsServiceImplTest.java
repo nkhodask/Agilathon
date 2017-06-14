c93bd68540041f767b53784faeaee9eb75a12e2f
niket.anup.khodaskar <Niket.Khodaskar@aexp.com>
2017-06-14 12:00:56 +0530

    updated junit

--- src/test/java/com/demo/policy/policydetails/service/FetchPolicyDetailsServiceImplTest.java
+++ src/test/java/com/demo/policy/policydetails/service/FetchPolicyDetailsServiceImplTest.java
@@ -1,19 +1,46 @@
 package com.demo.policy.policydetails.service;
 
+import com.demo.policy.policydetails.jpa.FetchPolicyDetailsRepository;
+import com.demo.policy.policydetails.jpa.PolicyDetailsEntity;
+import org.easymock.EasyMockSupport;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.junit.Assert.assertTrue;
 
 /**
  * Created by b.mukeshlal.raghani on 6/12/2017.
  */
-public class FetchPolicyDetailsServiceImplTest {
+public class FetchPolicyDetailsServiceImplTest extends EasyMockSupport {
+    private FetchPolicyDetailsServiceImpl fetchPolicyDetailsServiceImpl;
+
+    private FetchPolicyDetailsRepository fetchPolicyDetailsRepository;
+
+    @Before
+    public void setUp()
+    {
+        fetchPolicyDetailsServiceImpl = new FetchPolicyDetailsServiceImpl();
+        fetchPolicyDetailsRepository = createStrictMock(FetchPolicyDetailsRepository.class);
+        ReflectionTestUtils.setField(fetchPolicyDetailsServiceImpl,"fetchPolicyDetailsRepository",fetchPolicyDetailsRepository);
+    }
 
     @Test
-    public void testSampleTestCase()
+    public void testFetchDetails()
     {
-        boolean result = true;
+        fetchPolicyDetailsServiceImpl.fetchDetails(1L);
+    }
 
-        assertTrue(result);
+    @Test
+    public void testAddPolicy()
+    {
+        PolicyDetailsEntity policyDetailsEntity = new PolicyDetailsEntity();
+        fetchPolicyDetailsServiceImpl.addPolicy(policyDetailsEntity);
+    }
+
+    @Test
+    public void testDeletePolicy()
+    {
+        fetchPolicyDetailsServiceImpl.deletePolicy(1L);
     }
 }
